ca7d20b7a7098210e3bf7a4d4f0fb6c48d1d487f
Yurii <juretz32@yandex.ru>
2022-06-13 16:34:50 +0300

    Parametrize install script

--- install.sh
+++ install.sh
@@ -1,15 +1,41 @@
 set -e
-webinf="${1?'Usage: ./install <path-to-web-inf>'}"
+
+
+usage="usage: `basename $0` [-t <type>] [-p <port>] <path-to-web-inf>"
+type=nrepl
+port=7777
+
+
+while getopts :t:p:h OPT; do
+    case $OPT in
+        t)
+            type=$OPTARG
+            ;;
+        p)
+            port=$OPTARG
+            ;;
+        *)
+            echo $usage
+            exit 0
+    esac
+done
+shift $(($OPTIND - 1))
+
+
+webinf="$1"
+[ "$webinf" ] || { echo $usage; exit 0; }
 webinf=$(readlink -f "$webinf")
 
 
 cd "$(readlink -f $(dirname $0))"
 
 
+echo "Installing jar file..."
 cp -v target/*-jar-with-dependencies.jar "$webinf/lib/repl.jar"
 
 
-printf "Install web.xml settings..."
+printf "Installing servlet config..."
 sed -i '/<!--REPL servlet-->/,/<\/servlet>/ d' "$webinf/web.xml"
 sed -i '/<\/welcome-file-list>/ r servlet.xml' "$webinf/web.xml"
+sed -i 's:${type}:'$type':; s:${port}:'$port':' "$webinf/web.xml"
 echo ' done'
